acbe409b36a9aa4b49557c057e334b3d7e67c4f3
tobias <tobiassm12@hotmail.dk>
2024-03-09 18:37:48 +0100

    Quick Push

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 # Implement quicksort for me: https://en.wikipedia.org/wiki/Quicksort
 def quicksort(arr):
-
-        "Hello"
     if len(arr) <= 1:
         return arr
     pivot = arr[len(arr) // 2]
@@ -9,3 +7,4 @@ def quicksort(arr):
     middle = [x for x in arr if x == pivot]
     right = [x for x in arr if x > pivot]
     return quicksort(left) + middle + quicksort(right)
+
